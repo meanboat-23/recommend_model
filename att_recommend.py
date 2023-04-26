af3d9a00d84f4c8313f052a50322e25b9351a9b2
meanboat-23 <97111392+meanboat-23@users.noreply.github.com>
2023-04-26 17:24:06 +0900

    Add files via upload
    fix some code

--- att_recommend.py
+++ att_recommend.py
@@ -13,7 +13,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 import json
 
-def att_recommend(attraction_name = '', path = ''):
+def att_recommend(attraction_name = 'user', path = '', input_keyword=''):
   final_data = pd.read_csv(path)
 
   label = ['간사이 공항', '오사카 국제공항']
@@ -25,16 +25,21 @@ def att_recommend(attraction_name = '', path = ''):
   final_data_1 = final_data.drop(index, axis=0)
   final_data_1 = final_data_1.reset_index()
 
+  df1 = pd.DataFrame(columns=range(2))
+  df1.columns = ['Name', 'keyword']
+
+  df1.Name = final_data_1.Name
+  df1.keyword = final_data_1.keyword
+  df1.loc[len(df1)] = ['user', input_keyword]
+
   count_vect = CountVectorizer(min_df=0, ngram_range=(1,2))
-  matrixs = count_vect.fit_transform(final_data_1['keyword'])
+  matrixs = count_vect.fit_transform(df1['keyword'])
 
   cosine_matrix = cosine_similarity(matrixs, matrixs)
 
-  cosine_matrix
-
   # 관광지 title와 id를 매핑할 dictionary를 생성해줍니다.
   att2id = {}
-  for i, c in enumerate(final_data_1['Name']): att2id[i] = c
+  for i, c in enumerate(df1['Name']): att2id[i] = c
 
   # id와 관광지 title를 매핑할 dictionary를 생성해줍니다.
   id2att = {}
@@ -43,11 +48,13 @@ def att_recommend(attraction_name = '', path = ''):
   idx = id2att[attraction_name]
   sim_scores = [(i, c) for i, c in enumerate(cosine_matrix[idx]) if i != idx] # 자기 자신을 제외한 관광지들의 유사도 및 인덱스를 추출
   sim_scores = sorted(sim_scores, key = lambda x: x[1], reverse=True) # 유사도가 높은 순서대로 정렬
-  sim_scores[0:10] # 상위 10개의 인덱스와 유사도를 추출
-
   sim_scores = [(att2id[i], score) for i, score in sim_scores[0:10]]
 
   attraction = []
+
+  for i in range(len(sim_scores)):
+    box = list(sim_scores[i])
+    attraction.append(box[0])
 
   return attraction
 
